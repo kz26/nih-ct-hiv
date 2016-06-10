4edb531ee732dab4d2c82369bcd671a65279eb65
Kevin Zhang <whitehat2k9@gmail.com>
2016-06-10 11:56:58 -0400

    Implement argparse for manual_annotator.py

--- manual_annotator.py
+++ manual_annotator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import sqlite3
 import sys
@@ -7,61 +8,80 @@ import webbrowser
 from time import sleep
 
 
-def _save_annotation(c, id, value):
-    c.execute("INSERT OR REPLACE INTO hiv_status VALUES(?, ?)", [id, value])
+class Database(object):
+    def __init__(self, path):
+        super().__init__()
+        self.conn = sqlite3.connect(path)
+        self.counter = 0
 
+    def save_annotation(self, id, value, commit=True):
+        self.conn.execute("INSERT OR REPLACE INTO hiv_status VALUES(?, ?)", [id, value])
+        if commit:
+            self.conn.commit()
 
-def annotate_interactive(conn, id, counter, allow_skip=False):
-    c = conn.cursor()
-    c.execute("SELECT BriefTitle, StudyType, EligibilityCriteria FROM studies WHERE NCTId=?", [id])
-    row = c.fetchone()
-    print('-' * 40)
-    print(id)
-    print('\n'.join(row))
-    print('-' * 40)
-    v = None
-    if allow_skip:
-        prompt = 'Enter y/n/i/b/s --> '
-    else:
-        prompt = 'Enter y/n/i/b/s --> '
-    print_status(conn, counter)
-    while v is None:
-        rv = input(prompt).lower()
-        if rv == 'y':
-            v = 2
-        elif rv == 'n':
-            v = 0
-        elif rv == 'i':
-            v = 1
-        elif rv == 'b':
-            v = None
-            webbrowser.open("https://clinicaltrials.gov/ct2/show/%s" % id)
-        elif allow_skip and rv == 's':
-            return None
-    _save_annotation(c, id, v)
-    value = v
-    conn.commit()
-    return value
+    def prompt_for_annotation(self, id, content, allow_skip=False):
+        print('-' * 40)
+        print(id)
+        print('\n'.join(content))
+        print('-' * 40)
+        v = None
+        if allow_skip:
+            prompt = 'Enter y/n/i/b/s --> '
+        else:
+            prompt = 'Enter y/n/i/b/s --> '
+        self.print_status()
+        while v is None:
+            rv = input(prompt).lower()
+            if rv == 'y':
+                v = 2
+            elif rv == 'n':
+                v = 0
+            elif rv == 'i':
+                v = 1
+            elif rv == 'b':
+                v = None
+                webbrowser.open("https://clinicaltrials.gov/ct2/show/%s" % id)
+            elif allow_skip and rv == 's':
+                return None
+        self.save_annotation(id, v)
+        value = v
+        self.counter += 1
+        return value
 
+    def print_status(self):
+        c = self.conn.cursor()
+        c.execute('SELECT COUNT(*) FROM hiv_status')
+        total = c.fetchone()[0]
+        c.execute('SELECT COUNT(*) FROM hiv_status WHERE hiv_eligible=2')
+        eligible = c.fetchone()[0]
+        c.execute('SELECT COUNT(*) FROM hiv_status WHERE hiv_eligible=1')
+        implicit = c.fetchone()[0]
+        print("Annotated %s this session, %s total, %s eligible, %s implicit" %
+              (self.counter, total, eligible, implicit))
 
-def print_status(conn, counter):
-    c = conn.cursor()
-    c.execute('SELECT COUNT(*) FROM hiv_status')
-    total = c.fetchone()[0]
-    c.execute('SELECT COUNT(*) FROM hiv_status WHERE hiv_eligible=2')
-    eligible = c.fetchone()[0]
-    c.execute('SELECT COUNT(*) FROM hiv_status WHERE hiv_eligible=1')
-    implicit = c.fetchone()[0]
-    print("Annotated %s this session, %s total, %s eligible, %s implicit" % (counter, total, eligible, implicit))
+    def annotate_interactive(self):
+        c = self.conn.cursor()
+        c.execute('SELECT NCTId, BriefTitle, Condition, StudyType, EligibilityCriteria FROM studies \
+                  WHERE NOT EXISTS(SELECT * FROM hiv_status \
+                                   WHERE studies.NCTId=hiv_status.NCTId) ORDER BY random()')
+        for row in c.fetchall():
+            self.prompt_for_annotation(row[0], [row[1], row[2].replace('\n', ', '), row[3], row[4]], allow_skip=True)
+            sleep(0.5)
+            os.system('clear')
 
 if __name__ == '__main__':
-    conn = sqlite3.connect(sys.argv[1])
-    c = conn.cursor()
 
-    c.execute('SELECT NCTId FROM studies WHERE NOT EXISTS(SELECT * FROM hiv_status WHERE studies.NCTId=hiv_status.NCTId) ORDER BY random()')
-    i = 0
-    for row in c.fetchall():
-        annotate_interactive(conn, row[0], i, allow_skip=True)
-        i += 1
-        sleep(0.5)
-        os.system('clear')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', metavar='FILE', dest='db_path', default='studies.sqlite',
+                        help='SQLite database file to use')
+    subparsers = parser.add_subparsers(dest='subcmd', title='subcommand')
+    subparsers.required = True
+
+    parser_interactive = subparsers.add_parser('interactive',
+                                               help='annotate unlabeled studies from the database interactively')
+    ns = parser.parse_args()
+    db = Database(ns.db_path)
+    if ns.subcmd == 'interactive':
+        db.annotate_interactive()
+
+
