2388f249f05760c145e85e05087193472bcf675d
Kevin Zhang <whitehat2k9@gmail.com>
2016-06-10 12:30:07 -0400

    Add cherry-pick function; add a few more positive studies

--- manual_annotator.py
+++ manual_annotator.py
@@ -2,11 +2,14 @@
 
 import argparse
 import os
+import requests
 import sqlite3
 import sys
 import webbrowser
 from time import sleep
+import xml.etree.ElementTree as ET
 
+API_URL = "https://clinicaltrials.gov/ct2/show/%s?displayxml=true"
 
 class Database(object):
     def __init__(self, path):
@@ -65,9 +68,25 @@ class Database(object):
                   WHERE NOT EXISTS(SELECT * FROM hiv_status \
                                    WHERE studies.NCTId=hiv_status.NCTId) ORDER BY random()')
         for row in c.fetchall():
-            self.prompt_for_annotation(row[0], [row[1], row[2].replace('\n', ', '), row[3], row[4]], allow_skip=True)
+            self.prompt_for_annotation(row[0], (row[1], row[2].replace('\n', ', '), row[3], row[4]), allow_skip=True)
             sleep(0.5)
             os.system('clear')
+
+    def cherry_pick(self, study_id):
+        r = requests.get(API_URL % study_id)
+        root = ET.fromstring(r.text)
+        values = [None, None, None, study_id]
+        values.append(root.find('overall_status').text)  # values[4]
+        values.append(root.find('brief_title').text)
+        values.append(root.find('condition').text)
+        values.append(root.find('intervention').find('intervention_name').text)
+        values.append(root.find('eligibility').find('criteria').find('textblock').text)
+        values.append(root.find('study_type').text)
+        plc = ', '.join(['?'] * len(values))
+        self.conn.execute("INSERT OR REPLACE INTO studies VALUES(%s)" % plc, values)
+        self.prompt_for_annotation(study_id,
+                                   (values[5], values[6].replace('\n', ', '), values[7], values[4], values[8]))
+        self.print_status()
 
 if __name__ == '__main__':
 
@@ -79,9 +98,14 @@ if __name__ == '__main__':
 
     parser_interactive = subparsers.add_parser('interactive',
                                                help='annotate unlabeled studies from the database interactively')
+    parser_cherry_pick = subparsers.add_parser('cherry-pick', help='add and annotate a study from ClinicalTrials.gov')
+    parser_cherry_pick.add_argument('study_id', help='NCTID identifier from ClinicalTrials.gov')
+
     ns = parser.parse_args()
     db = Database(ns.db_path)
     if ns.subcmd == 'interactive':
         db.annotate_interactive()
+    elif ns.subcmd =='cherry-pick':
+        db.cherry_pick(ns.study_id)
 
 
